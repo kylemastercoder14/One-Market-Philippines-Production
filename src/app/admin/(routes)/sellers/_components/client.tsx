ec559e3b65cd2e4e0389c710cfdd3901ef1f9735
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-02-10 17:45:43 +0800

    added id ui

--- src/app/admin/(routes)/sellers/_components/client.tsx
+++ src/app/admin/(routes)/sellers/_components/client.tsx
@@ -11,7 +11,7 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 import React from "react";
-import { Clock, Search } from "lucide-react";
+import { Clock, DownloadIcon, EyeIcon, Search } from "lucide-react";
 import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 
@@ -32,6 +32,8 @@ const StoreClient = ({
 }) => {
   const image =
     "https://img.kwcdn.com/supplier-public-tag/1f13e183980/58186005-2bcb-4c92-97c0-814cf3cbf508_300x300.jpeg?imageView2/2/w/800/q/70/format/webp";
+  const identityUrl = seller?.identity || "";
+  const fileName = identityUrl.split("/").pop();
   return (
     <>
       <div className="flex items-center py-2 justify-between">
@@ -44,7 +46,14 @@ const StoreClient = ({
             className="rounded-full object-cover"
           />
           <div>
-            <h1 className="font-semibold text-lg flex items-center gap-3">{seller?.name} <Badge className="bg-black hover:bg-black/90">Pro Account</Badge></h1>
+            <div className="font-semibold text-lg flex items-center gap-3">
+              {seller?.name}{" "}
+              {seller?.isPremium && (
+                <Badge className="bg-black hover:bg-black/90">
+                  Pro Account
+                </Badge>
+              )}
+            </div>
             <p className="text-muted-foreground">
               Business Type: {seller?.type}
             </p>
@@ -80,14 +89,14 @@ const StoreClient = ({
           <TabsContent value="info">
             <div className="flex w-full mt-3 flex-col">
               <h1 className="font-semibold">Seller Information</h1>
-              <div className="flex min-w-[450px] items-center justify-between rounded-md mt-3 py-2 px-3 bg-zinc-200">
+              <div className="flex min-w-[400px] items-center justify-between rounded-md mt-3 py-2 px-3 bg-zinc-200">
                 <div className="flex items-center gap-2 w-full">
                   <div className="bg-black rounded-md p-2">
-                    <Clock className="w-4 h-4 text-white" />
+                    <Clock className="w-3 h-3 text-white" />
                   </div>
                   <span className="text-sm font-semibold">Date Created:</span>
                 </div>
-                <span className="w-full">
+                <span className="w-full text-sm">
                   {seller?.createdAt
                     ? format(
                         new Date(seller.createdAt),
@@ -96,7 +105,34 @@ const StoreClient = ({
                     : "N/A"}
                 </span>
               </div>
-              
+              <h1 className="font-semibold mt-4">Attachments</h1>
+              <div className="flex flex-col mt-3">
+                <div className="flex items-center gap-2">
+                  <div className="relative">
+                    <Image
+                      src={seller?.identity || ""}
+                      alt="ID"
+                      width={80}
+                      height={80}
+                      className="rounded-lg"
+                    />
+                    <div className="absolute text-xs bg-black/80 p-1 rounded-md text-white top-0 right-0">
+                      {seller?.identityType}
+                    </div>
+                  </div>
+                  <div>
+                    <span className="text-sm">{fileName}</span>
+                    <div className="flex items-center">
+                      <Button variant="ghost" size="sm">
+                        <EyeIcon className="w-4 h-4" /> View
+                      </Button>
+                      <Button variant="ghost" size="sm">
+                        <DownloadIcon className="w-4 h-4" /> Download
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </TabsContent>
           <TabsContent value="items">Change your password here.</TabsContent>

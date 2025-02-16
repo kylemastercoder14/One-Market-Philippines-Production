9be648a48a7d72d0940eb058d1bd81451e4b6c49
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-02-16 13:22:35 +0800

    removed delete api temporary

--- src/app/api/[sellerId]/orders/[orderId]/route.ts
+++ src/app/api/[sellerId]/orders/[orderId]/route.ts
@@ -18,18 +18,18 @@ export async function PATCH(
 }
 
 // This is the route file for the orders endpoint. It is used for deleting an existing order for a specific seller.
-export async function DELETE(
-  req: Request,
-  { params }: { params: { sellerId: string; orderId: string } }
-) {
-  try {
-    if (!params.sellerId || !params.orderId) {
-      return new NextResponse("Seller ID and Order ID are required", {
-        status: 400,
-      });
-    }
-  } catch (error) {
-    console.error("[SELLERID_DELETE_ORDERID_ERROR]", error);
-    return new NextResponse("Internal server error", { status: 500 });
-  }
-}
+// export async function DELETE(
+//   req: Request,
+//   { params }: { params: { sellerId: string; orderId: string } }
+// ) {
+//   try {
+//     if (!params.sellerId || !params.orderId) {
+//       return new NextResponse("Seller ID and Order ID are required", {
+//         status: 400,
+//       });
+//     }
+//   } catch (error) {
+//     console.error("[SELLERID_DELETE_ORDERID_ERROR]", error);
+//     return new NextResponse("Internal server error", { status: 500 });
+//   }
+// }

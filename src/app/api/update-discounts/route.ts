39deedeba7c504b2ebadbf07171c4356d5e8cf76
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-02-16 14:04:51 +0800

    removed delete api temporary

--- src/app/api/update-discounts/route.ts
+++ src/app/api/update-discounts/route.ts
@@ -1,41 +1,36 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import db from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const now = new Date();
+export async function POST(req: Request) {
+  const now = new Date();
 
-    try {
-      // Find and update expired discounts
-      const expiredDiscounts = await db.sellerDiscount.findMany({
-        where: {
-          endDate: { lt: now.toISOString() }, // endDate is less than the current date
-          status: "active", // Only update active discounts
-        },
+  try {
+    // Find and update expired discounts
+    const expiredDiscounts = await db.sellerDiscount.findMany({
+      where: {
+        endDate: { lt: now.toISOString() }, // endDate is less than the current date
+        status: "active", // Only update active discounts
+      },
+    });
+
+    // Update the status of expired discounts to "Inactive"
+    for (const discount of expiredDiscounts) {
+      await db.sellerDiscount.update({
+        where: { id: discount.id },
+        data: { status: "Inactive" },
       });
-
-      // Update the status of expired discounts to "Inactive"
-      for (const discount of expiredDiscounts) {
-        await db.sellerDiscount.update({
-          where: { id: discount.id },
-          data: { status: "Inactive" },
-        });
-        console.log(`Updated discount ${discount.id} to Inactive.`);
-      }
-
-      res
-        .status(200)
-        .json({ message: "Expired discounts updated successfully." });
-    } catch (error) {
-      console.error("Error updating expired discounts:", error);
-      res.status(500).json({ error: "Failed to update expired discounts." });
-    } finally {
-      await db.$disconnect();
+      console.log(`Updated discount ${discount.id} to Inactive.`);
     }
-  } else {
-    res.status(405).json({ error: "Method not allowed." });
+
+    return new NextResponse("Updated expired discounts successfully.", {
+      status: 201,
+    });
+  } catch (error) {
+    console.error("Error updating expired discounts:", error);
+    return new NextResponse("Failed to update expired discounts.", {
+      status: 500,
+    });
+  } finally {
+    await db.$disconnect();
   }
 }

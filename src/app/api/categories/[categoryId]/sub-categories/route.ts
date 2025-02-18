f2e590ec1bfbb7b1bbd1ac2c485773d68d1e463f
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-02-19 07:06:45 +0800

    updated route api

--- src/app/api/categories/[categoryId]/sub-categories/route.ts
+++ src/app/api/categories/[categoryId]/sub-categories/route.ts
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import db from "@/lib/db";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { categoryId: string } }
-) {
+interface RequestContext {
+  params: { categoryId: string };
+}
+
+export async function GET(req: NextRequest, { params }: RequestContext) {
   try {
-    const { categoryId } = context.params;
+    const { categoryId } = params;
 
     if (!categoryId) {
       return new NextResponse("Category ID is required", { status: 400 });
